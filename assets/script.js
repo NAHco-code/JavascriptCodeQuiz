66ef1b70bda6725a8bbf11c05a741a4aaa48e943
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-02-22 15:57:11 -0600

    Function + Style Build: complete functions + add styling within functions for more dynamic user experience
    Complete renderQuestion function by adding choiceSelected variable on click; define choiceSelected function by assigning all choices for renderedQuestion to choice_list div, and adding css selectors to display whether user picks correct or incorrect answer +  disbaling pointer events once a choice is selected. Define questionCounter function by linking countertext to total_questions div, and specifying where counter text will display within the div.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,31 @@
-//global variables 
+//global 
 var quiz = [{ //quiz object
-    question: 1,
+    number: 1,
+    question: "question 1",
     choices: ["a option ", "correct ", "c option ", "d option "],
     answer: "correct "
     },
     {
-    question: 2,
+    number: 2,
+    question: "question 2",
     choices: ["a option ", "b option ", "c option ", "correct "],
     answer: "correct "
     },
     {
-    question: 3,
+    number: 3,
+    question: "question 3",
     choices: ["a option ", "b option ", "correct ", "d option "],
     answer: "correct "
     },
     {
-    question: 4,
+    number: 4,
+    question: "question 4",
     choices: ["correct ", "b option ", "c option ", "d option "],
     answer: "correct "
     },
     {
-    question: 5,
+    number: 5,
+    question: "question 5",
     choices: ["a option ", "correct ", "c option ", "d option "],
     answer: "correct "
     },
@@ -35,7 +40,7 @@ const infoBox = document.querySelector('#info_box');
 const quizBox = document.querySelector('#quiz_box');
 
 const questionEl = document.querySelector('#questions');
-const choicesEl = document.querySelector('#choice_list');
+const choiceList = document.querySelector('#choice_list');
 
 
 //if start button clicked
@@ -52,33 +57,66 @@ exitBtn.onclick = ()=>{
 continueBtn.onclick = ()=>{
     infoBox.classList.remove("activeInfo"); //hide info box
     quizBox.classList.add("activeQuiz"); //show quiz box
-    renderQuestion(0);
+    renderQuestion(0); //question in position 1
+    questionCounter(1); //start question count at 1
 }
 
 let questionCount = 0;//start with question in position 1
-//if next button is clicked
+let questionNumber = 1; //start with question 1
 
-nxt_btn.onclick = () => {
+nxt_btn.onclick = () => { //if next button is clicked
     if (questionCount < quiz.length - 1) {
-        questionCount++;
-        renderQuestion(questionCount);
-    } else {
+        questionCount++; //increase question count
+        questionNumber++; //increase number count
+        renderQuestion(questionCount); //call both funtions simultaneously
+        questionCounter(questionNumber);
+    } else { // if the question count is not less than the quiz length
         console.log("You've completed the Quiz!");
     }    
 }
 
+// getting questions and choices from array
 function renderQuestion(index) { //define renderQuestion function
     questionEl.innerHTML = ''; //clear questions div = allows content of div to be replaced with each new question and it's contents
-    let questionText = '<span>' + quiz[index].question + '</span>';
-    let choicesText =
-        '<div class="choice_item"><span>' + quiz[index].choices[0] + '</span></div>'
+    let questionText = '<span>' + quiz[index].number + ". " + quiz[index].question + '</span>'; // specify where question text and question number will display - pulling from quiz object index
+    let choicesText = // specify where choices text will display - pulling from quiz object index
+          '<div class="choice_item"><span>' + quiz[index].choices[0] + '</span></div>'
         + '<div class="choice_item"><span>' + quiz[index].choices[1] + '</span></div>'
         + '<div class="choice_item"><span>' + quiz[index].choices[2] + '</span></div>'
         + '<div class="choice_item"><span>' + quiz[index].choices[3] + '</span></div>';
     
-    questionEl.innerHTML = questionText;
-    choicesEl.innerHTML = choicesText;
-    // questionEl.appendChild(questionTextEl, choicesEl); //attach text content of current question index to questions div
+    questionEl.innerHTML = questionText; //set question text to display in DOM
+    choiceList.innerHTML = choicesText; //set choices text to display in each choice element in 
+
+    //set new constant variable choice to link to each choice item in DOM
+    const choice = choiceList.querySelectorAll(".choice_item");
+    for (let i = 0; i < choice.length; i++) { //when a choice is clicked - set choice user clicks to 'choice selected' 
+        choice[i].setAttribute("onclick", "choiceSelected(this)");
+    }
+}
+
+function choiceSelected(answer) { //define 'choice selected' function, and compare with correct answer
+    let userAnswer = answer.textContent;
+    let correctAnswer = quiz[questionCount].answer;
+    let allChoices = choiceList.children.length; //assign 'all choices' variable to length of choice items (which are children of 'choice list')
+    if (userAnswer == correctAnswer) {
+        answer.classList.add("correct"); //add css selector to show green if correct answer is selected
+        console.log("Answer is Correct"); 
+    } else {
+        answer.classList.add("incorrect"); //add css selector to show red if wrong selection is made
+        console.log("Answer is Wrong");
+    }
+    
+    for (let i = 0; i < allChoices; i++) {// once user selects, disable all options
+        choiceList.children[i].classList.add("disabled"); //disable pointer events when a selection is made
+    }
+}
+
+
+function questionCounter(index) { //define questionCounter function using quiz object index
+    const bottomQuestionCounter = quizBox.querySelector(".total_questions"); //link bottomQuestionCounter variable to total_questions div
+    let totalQuesCountText = '<span><p>' + questionCount + '</p>of<p>' + quiz.length + '</p>Questions</span>'; // specify where question count will appear
+    bottomQuestionCounter.innerHTML = totalQuesCountText; 
 }
     // choicesEl.addEventListener('click', (event) => { // add click event to all choices
     //     alert('you clicked the ' + (event.target.dataset.correct === 'true' ? 'correct' : 'wrong!') + ' button') // true false statement to evaluate if button the user clicked is correct
